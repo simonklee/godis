d1ac86939e519a21e00e44cb27ea89773ee501b5
Simon Zimmermann <simonz05@gmail.com>
2011-04-07 15:43:19 +0200

    add example

---
+++ example/strings.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "github.com/simonz05/godis"
+    "fmt"
+)
+
+func main() {
+    // new client on default IP/port, redis db to 0 and no password
+    c := godis.New("127.0.0.1:6379", 0, "") 
+
+    // set a "foo" to "bar" 
+    godis.Set(c, "foo", "bar")
+
+    // retrieve the value of "foo"
+    foo, _ := godis.Get(c, "foo")
+
+    // convert return value back to string and print it
+    fmt.Println("foo: ", foo.String())
+}
