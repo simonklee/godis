7df3540b41a41b2d7f9896828fceecf894982045
Simon Zimmermann <simonz05@gmail.com>
2011-05-04 14:50:13 +0200

    update examples

---
+++ example/list.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "godis"
+    "fmt"
+    "os"
+)
+
+func main() {
+    // new client on default port 6379, select db 0 and use no password.
+    c := godis.New("", 0, "")
+
+    // values we want to store
+    values := []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}
+
+    // push the values to the redis list 
+    for _, v := range values {
+        if _, err := c.Rpush("bar", v); err != nil {
+            fmt.Fprintln(os.Stderr, err)
+            os.Exit(1)
+        }
+    }
+
+    // retrieve the items in the list. Returns a Reply object.
+    res, _ := c.Lrange("bar", 0, 9)
+
+    // convert the list to an array of ints and print it.
+    fmt.Println("bar:", res.IntArray())
+}
---
+++ example/string.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "godis"
+    "fmt"
+    "os"
+)
+
+func main() {
+    // new client on default port 6379, select db 0 and use no password
+    c := godis.New("", 0, "") 
+
+    // set the key "foo" to "Hello Redis"
+    if err := c.Set("foo", "Hello Redis"); err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+
+    // retrieve the value of "foo". Returns an Elem obj
+    elem, _ := c.Get("foo")
+
+    // convert the obj to a string and print it 
+    fmt.Println("foo:", elem.String())
+}
